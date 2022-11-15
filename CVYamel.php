a764293b10bfb57aa53c0492facac148326c4309
SoftYam <46936213+SoftYam@users.noreply.github.com>
2022-11-15 15:22:24 -0400

    Add files via upload

---
+++ CVYamel.php
@@ -0,0 +1,28 @@
+<DOCTYPE html>
+<html>
+<head>
+  <title<CV de Yamel Hurtado</title>
+  <meta charset="utf-8">
+  <meta name="description" content=Yamel est{a aprendiendo a Desarrollar Web con codigofacilito">
+  </head>
+  <body style="font-family: Helvetica, arial, sans-serif;margin: 0;">
+    <header style="background-color:green;color:white;text-align:center;padding:16px;">
+	  <h1 style="display:inline-block;width:400px;font-weight:700">CV de Yamel Hurtado</h1>
+	  <h1 style="display:inline-block;width:400px;font-weight:700">Desarrollador Web</h1>
+	  </header>
+	  <div> style=background-color:blue;color:black;text-align:center;padding:10px;">
+	  Mejorando tecnicas de Desarrollo Web.
+	  </div>
+
+	  <section>
+	  <div style="width:960px;margin-rigth:auto;margin-left:auto;">
+	  <div>
+	    <dl>
+	       <dt style="font-size:22px;text-transform:uppercase;margin-bottom:12px;margin-top:39px;">Ingeniero en Inform&atica>/dt>
+
+
+
+
+		 </div>
+		 </section>
+
